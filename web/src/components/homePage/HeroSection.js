3e818e794ced3db2f6df90b96206f5b7b59939b7
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-11-25 11:15:25 -0300

    modificacion hero section

--- web/src/components/homePage/HeroSection.js
+++ web/src/components/homePage/HeroSection.js
@@ -11,7 +11,7 @@ function HeroSection() {
       <div className="container">
         <div className="hero__wrapper">
           <div className="left">
-            <h1 className="hero__heading">Portfolio Néstor Santiago Fuhr</h1>
+            <h1 className="hero__heading">Portfolio Néstor Santiago Fuhr II</h1>
             <ParagraphText className="hero__text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit Lorem
               ipsum dolor sit amet consectetur adipisicing elit
